8dfd52119b4b8a1de90814c774b187e4b100b8c0
Martin <martin.soethe@gmx.de>
2016-11-27 17:13:31 +0100

    Restructure renpass_gis in main

--- renpass_gis_main.py
+++ renpass_gis_main.py
@@ -58,104 +58,122 @@ def stopwatch():
     stopwatch.now = datetime.now()
     return str(stopwatch.now-last)[0:-4]
 
-logger.define_logging()
+
+def main(**arguments):
 
 
-# %% model creation and solving
-
-es = EnergySystem(groupings=GROUPINGS, timeindex=datetime_index)
-
-nodes = NodesFromCSV(file_nodes_flows=os.path.join(
-                         scenario_path, nodes_flows),
-                     file_nodes_flows_sequences=os.path.join(
-                         scenario_path, nodes_flows_sequences),
-                     delimiter=',')
-
-stopwatch()
-
-om = OperationalModel(es)
-
-logging.info('OM creation time: ' + stopwatch())
-
-om.receive_duals()
-
-om.solve(solver='glpk', solve_kwargs={'tee': True})
-
-logging.info('Optimization time: ' + stopwatch())
-
-logging.info('Done! \n Check the results')
 
 
-# %% output: create pandas dataframe with results
 
-results = ResultsDataFrame(energy_system=es)
+    # %% misc.
 
+    stopwatch()
 
-# %% postprocessing: write complete result dataframe to file system
+    datetime_index = pd.date_range(arguments['--date-from'],
+                                   arguments['--date-to'],
+                                   freq='60min')
 
-if not os.path.isdir('results'):
-    os.mkdir('results')
+    # %% model creation and solving
 
-results_path = 'results'
+    es = EnergySystem(groupings=GROUPINGS, time_idx=datetime_index)
 
-date = str(datetime.now())
+    nodes = NodesFromCSV(file_nodes_flows=os.path.join(
+                         arguments['--path'], arguments['NODESFLOWS']),
+                         file_nodes_flows_sequences=os.path.join(
+                         arguments['--path'],
+                         arguments['NODESFLOWSSEQ']),
+                         delimiter=',')
 
-file_name = 'scenario_' + nodes_flows.replace('.csv', '_') + date + '_' + \
-            'results_complete.csv'
+    om = OperationalModel(es)
 
-results.to_csv(os.path.join(results_path, file_name))
+    logging.info('OM creation time: ' + stopwatch())
 
+    om.receive_duals()
 
-# %% postprocessing: write dispatch and prices for all regions to file system
+    om.solve(solver=arguments['--solver'], solve_kwargs={'tee': True})
 
-# country codes
-country_codes = ['AT', 'BE', 'CH', 'CZ', 'DE', 'DK', 'FR', 'LU', 'NL', 'NO',
-                 'PL', 'SE']
+    logging.info('Optimization time: ' + stopwatch())
 
-for cc in country_codes:
-    # build single dataframe for electric buses
-    inputs = results.slice_unstacked(bus_label=cc + '_bus_el', type='to_bus',
-                                     date_from=date_from, date_to=date_to,
-                                     formatted=True)
+    logging.info('Done! \n Check the results')
 
-    outputs = results.slice_unstacked(bus_label=(cc + '_bus_el'),
-                                      type='from_bus', date_from=date_from,
-                                      date_to=date_to, formatted=True)
+    #  %% output: create pandas dataframe with results
 
-    other = results.slice_unstacked(bus_label=cc + '_bus_el', type='other',
-                                    date_from=date_from, date_to=date_to,
-                                    formatted=True)
+    results = ResultsDataFrame(energy_system=es)
 
-    # AT, DE and LU are treated as one bidding area
-    if cc == 'DE':
-        for c in ['DE', 'AT', 'LU']:
+    # %% postprocessing: write complete result dataframe to file system
+
+    if not os.path.isdir('results'):
+        os.mkdir('results')
+
+    results_path = 'results'
+
+    date = str(datetime.now())
+
+    file_name = 'scenario_' + arguments['NODESFLOWS'].replace('.csv', '_') +\
+        date + '_' + 'results_complete.csv'
+
+    results.to_csv(os.path.join(results_path, file_name))
+
+    # %% postprocessing: write dispatch and prices for all regions to file
+    # system
+
+    # country codes
+    country_codes = ['AT', 'BE', 'CH', 'CZ', 'DE', 'DK', 'FR', 'LU', 'NL',
+                     'NO', 'PL', 'SE']
+
+    for cc in country_codes:
+        # build single dataframe for electric buses
+        inputs = results.slice_unstacked(bus_label=cc + '_bus_el',
+                                         type='to_bus',
+                                         date_from=arguments['--date-from'],
+                                         date_to=arguments['--date-to'],
+                                         formatted=True)
+
+        outputs = results.slice_unstacked(bus_label=(cc + '_bus_el'),
+                                          type='from_bus',
+                                          date_from=arguments['--date-from'],
+                                          date_to=arguments['--date-to'],
+                                          formatted=True)
+
+        other = results.slice_unstacked(bus_label=cc + '_bus_el',
+                                        type='other',
+                                        date_from=arguments['--date-from'],
+                                        date_to=arguments['--date-to'],
+                                        formatted=True)
+
+        # AT, DE and LU are treated as one bidding area
+        if cc == 'DE':
+            for c in ['DE', 'AT', 'LU']:
+                # rename redundant columns
+                inputs.rename(columns={c + '_storage_phs':
+                                       c + '_storage_phs_out'},
+                              inplace=True)
+                outputs.rename(columns={c + '_storage_phs':
+                                        c + '_storage_phs_in'},
+                               inplace=True)
+                other.rename(columns={c + '_storage_phs':
+                                      c + '_storage_phs_level'},
+                             inplace=True)
+
+                # data from model in MWh
+                country_data = pd.concat([inputs, outputs, other], axis=1)
+        else:
             # rename redundant columns
-            inputs.rename(columns={c + '_storage_phs':
-                                   c + '_storage_phs_out'},
+            inputs.rename(columns={cc + '_storage_phs': cc +
+                                   '_storage_phs_out'},
                           inplace=True)
-            outputs.rename(columns={c + '_storage_phs':
-                                    c + '_storage_phs_in'},
+            outputs.rename(columns={cc + '_storage_phs': cc +
+                                    '_storage_phs_in'},
                            inplace=True)
-            other.rename(columns={c + '_storage_phs':
-                                  c + '_storage_phs_level'},
+            other.rename(columns={cc + '_storage_phs': cc +
+                                  '_storage_phs_level'},
                          inplace=True)
 
             # data from model in MWh
             country_data = pd.concat([inputs, outputs, other], axis=1)
-    else:
-        # rename redundant columns
-        inputs.rename(columns={cc + '_storage_phs': cc + '_storage_phs_out'},
-                      inplace=True)
-        outputs.rename(columns={cc + '_storage_phs': cc + '_storage_phs_in'},
-                       inplace=True)
-        other.rename(columns={cc + '_storage_phs': cc + '_storage_phs_level'},
-                     inplace=True)
 
-        # data from model in MWh
-        country_data = pd.concat([inputs, outputs, other], axis=1)
-
-    # sort columns and save as csv file
-    file_name = 'scenario_' + nodes_flows.replace('.csv', '_') + date + '_' + \
-                cc + '.csv'
-    country_data.sort_index(axis=1, inplace=True)
-    country_data.to_csv(os.path.join(results_path, file_name))
+        # sort columns and save as csv file
+        file_name = 'scenario_' + arguments['NODESFLOWS'].replace('.csv', '_')\
+            + date + '_' + cc + '.csv'
+        country_data.sort_index(axis=1, inplace=True)
+        country_data.to_csv(os.path.join(results_path, file_name))
