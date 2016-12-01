d7a07efb382b5294fcd3e32527963153190d02de
Soenke <soenke.bohm@uni-flensburg.de>
2016-12-01 12:51:48 +0100

    Update executable to work with v0.1

--- renpass_gis_main.py
+++ renpass_gis_main.py
@@ -1,4 +1,28 @@
 # -*- coding: utf-8 -*-
+""" renpass_gis
+
+Usage:
+  renpass_gis_main.py [options] NODE_DATA SEQ_DATA
+  renpass_gis_main.py -h | --help | --version
+
+Examples:
+
+  renpass_gis_main.py -o gurobi path/to/scenario.csv path/to/scenario-seq.csv
+
+Arguments:
+
+  NODE_DATA                  CSV-file containing data for nodes and flows.
+  SEQ_DATA                   CSV-file with data for sequences.
+
+Options:
+
+  -h --help                  Show this screen and exit.
+  -o --solver=SOLVER         Solver to be used. [default: cbc]
+     --output-directory=DIR  Directory to write results to. [default: results]
+     --date-from=TIMESTAMP   Start [default: '2014-01-01 00:00:00']
+     --date-to=TIMESTAMP     End interval. [default: '2014-12-31 23:00:00']
+     --version               Show version.
+"""
 
 import os
 import logging
@@ -9,24 +33,19 @@ from oemof.tools import logger
 from oemof.solph import OperationalModel, EnergySystem, GROUPINGS
 from oemof.solph import NodesFromCSV
 from oemof.outputlib import ResultsDataFrame
+from docopt import docopt
 
 
-# %% configuration
-
-scenario_path = 'scenarios/'
-
-date_from = '2050-01-01 00:00:00'
-date_to = '2050-12-31 23:00:00'
-
-nodes_flows = 'morocco_2050_scenario_test001.csv'
-nodes_flows_sequences = 'morocco_2050_seq.csv'
+###############################################################################
 
 
-# %% misc.
-
-datetime_index = pd.date_range(date_from, date_to, freq='60min')
-
-
+##arguments = {'NODE_DATA':'scenarios/morocco_2050_scenario_a.csv',
+     #        'SEQ_DATA':'scenarios/morocco_2050_seq.csv',
+      #       '--solver':'gurobi',
+       #      '--date-from':'2014-01-01 00:00:00',
+        #     '--date-to':'2014-01-31 23:00:00'}
+             
+             
 def stopwatch():
     if not hasattr(stopwatch, 'now'):
         stopwatch.now = datetime.now()
@@ -35,107 +54,193 @@ def stopwatch():
     stopwatch.now = datetime.now()
     return str(stopwatch.now-last)[0:-4]
 
-logger.define_logging()
+
+def create_nodes(**arguments):
+    """Creates nodes with their respective sequences
+
+    Parameters
+    ----------
+    **arguments : key word arguments
+        Arguments passed from command line
+    """
+    nodes = NodesFromCSV(file_nodes_flows=arguments['NODE_DATA'],
+                         file_nodes_flows_sequences=arguments['SEQ_DATA'],
+                         delimiter=',')
+
+    return nodes
 
 
-# %% model creation and solving
+def create_energysystem(nodes, **arguments):
+    """Creates the energysystem.
 
-es = EnergySystem(groupings=GROUPINGS, time_idx=datetime_index)
+    Parameters
+    ----------
+    nodes:
+        A list of entities that comprise the energy system
+    **arguments : key word arguments
+        Arguments passed from command line
+    """
 
+    datetime_index = pd.date_range(arguments['--date-from'],
+                                   arguments['--date-to'],
+                                   freq='60min')
 
+    es = EnergySystem(entities=nodes,
+                      groupings=GROUPINGS,
+                      timeindex=datetime_index)
 
-nodes = NodesFromCSV(file_nodes_flows=os.path.join(
-                         scenario_path, nodes_flows),
-                     file_nodes_flows_sequences=os.path.join(
-                         scenario_path, nodes_flows_sequences),
-                     delimiter=',')
-# print(es.entities)
-stopwatch()
-
-om = OperationalModel(es)
-
-logging.info('OM creation time: ' + stopwatch())
-
-om.receive_duals()
-
-om.solve(solver='gurobi', solve_kwargs={'tee': True})
-
-logging.info('Optimization time: ' + stopwatch())
-
-logging.info('Done! \n Check the results')
+    return es
 
 
-# %% output: create pandas dataframe with results
+def simulate(es=None, **arguments):
+    """Creates the optimization model, solves it and writes back results to
+    energy system object
 
-results = ResultsDataFrame(energy_system=es)
+    Parameters
+    ----------
+    es : :class:`oemof.solph.network.EnergySystem` object
+        Energy system holding nodes, grouping functions and other important
+        information.
+    **arguments : key word arguments
+        Arguments passed from command line
+    """
+
+    om = OperationalModel(es)
+
+    logging.info('OM creation time: ' + stopwatch())
+
+    om.receive_duals()
+
+    om.solve(solver=arguments['--solver'], solve_kwargs={'tee': True})
+
+    logging.info('Optimization time: ' + stopwatch())
+
+    return om
 
 
-# %% postprocessing: write complete result dataframe to file system
+def write_results(es, om, **arguments):
+    """Write results to CSV-files
 
-results_path = 'results/'
+    Parameters
+    ----------
+    es : :class:`oemof.solph.network.EnergySystem` object
+        Energy system holding nodes, grouping functions and other important
+        information.
+    om : :class:'oemof.solph.models.OperationalModel' object for operational
+        simulation with optimized dispatch
+    **arguments : key word arguments
+        Arguments passed from command line
 
-date = str(datetime.now())
-date_date = date[0:10]
-date_time = date[11:19]
-date_time = date_time.replace(":", "-", 2)
-date = date_date + '_' + date_time
+    """
+    # output: create pandas dataframe with results
 
-scenario_year = date_from[0:4]
-print("Scenario year: " + scenario_year)
+    results = ResultsDataFrame(energy_system=es)
 
-file_name = 'scenario_' + nodes_flows.replace('.csv', '_') + date + '_' + \
-            'results_complete.csv'
+    # postprocessing: write complete result dataframe to file system
 
-results.to_csv(os.path.join(results_path, file_name))
+    if not os.path.isdir(arguments['--output-directory']):
+        os.mkdir(arguments['--output-directory'])
+
+    results_path = arguments['--output-directory']
+
+    date = str(datetime.now())
+
+    #results_path = 'results/'
+
+    date = str(datetime.now())
+    date_date = date[0:10]
+    date_time = date[11:19]
+    date_time = date_time.replace(":", "-", 2)
+    date = date_date + '_' + date_time
+    
+    scenario_year = arguments['--date-from'][0:4]
+    print("Scenario year: " + scenario_year)
+    
+    file_name = 'scenario_' + os.path.basename(arguments['NODE_DATA']).replace('.csv', '_') + date + '_' + \
+                'results_complete.csv'
+    
+    results.to_csv(os.path.join(results_path, file_name))
+    
+    
+    #  postprocessing: write dispatch and prices for all regions to file system
+    
+    # country codes
+    country_codes = ['MAdr01', 'MAdr02', 'MAdr03', 'MAdr04', 'ES', 'AL', 'MR', 'PT']
+    
+    for cc in country_codes:
+    
+        # build single dataframe for electric busses
+        inputs = results.slice_unstacked(bus_label=cc + '_bus_el', type='to_bus',
+                                         date_from=arguments['--date-from'], date_to=arguments['--date-to'],
+                                         formatted=True)
+    
+        outputs = results.slice_unstacked(bus_label=cc + '_bus_el', type='from_bus',
+                                          date_from=arguments['--date-from'], date_to=arguments['--date-to'],
+                                          formatted=True)
+    
+        other = results.slice_unstacked(bus_label=cc + '_bus_el', type='other',
+                                        date_from=arguments['--date-from'], date_to=arguments['--date-to'],
+                                        formatted=True)
+    
+        # rename redundant columns
+        inputs.rename(columns={cc + '_storage_phs': cc + '_storage_phs_out'},
+                      inplace=True)
+        outputs.rename(columns={cc + '_storage_phs': cc + '_storage_phs_in'},
+                       inplace=True)
+        other.rename(columns={cc + '_storage_phs': cc + '_storage_phs_level'},
+                     inplace=True)
+        inputs.rename(columns={cc + '_reservoir': cc + '_storage_reservoir_out'},
+                      inplace=True)
+        outputs.rename(columns={cc + '_reservoir': cc + '_storage_reservoir_in'},
+                       inplace=True)
+        other.rename(columns={cc + '_reservoir': cc + '_storage_reservoir_level'},
+                     inplace=True)
+        inputs.rename(columns={cc + '_cspstorage': cc + '_storage_csp_out'},
+                      inplace=True)
+        outputs.rename(columns={cc + '_cspstorage': cc + '_storage_csp_in'},
+                       inplace=True)
+        other.rename(columns={cc + '_cspstorage': cc + '_storage_csp_level'},
+                     inplace=True)
+    
+        # data from model in MWh
+        country_data = pd.concat([inputs, outputs, other], axis=1)
+    
+        # sort columns and save as csv file
+        file_name = 'scenario_' + os.path.basename(arguments['NODE_DATA']).replace('.csv', '_') + date + '_' +\
+                    cc + '.csv'
+        country_data.sort_index(axis=1, inplace=True)
+        country_data.to_csv(os.path.join(results_path, file_name))
+        
+    return
 
 
-# %% postprocessing: write dispatch and prices for all regions to file system
+def main(**arguments):
+    """
+    """
+    logging.info('Starting renpass_gis!')
 
-# country codes
-country_codes = ['MAdr01', 'MAdr02', 'MAdr03', 'MAdr04', 'ES', 'AL', 'MR', 'PT']
+    stopwatch()
 
-for cc in country_codes:
+    # create nodes from csv
+    nodes = create_nodes(**arguments)
 
-    # build single dataframe for electric busses
-    inputs = results.slice_unstacked(bus_label=cc + '_bus_el', type='input',
-                                     date_from=date_from, date_to=date_to,
-                                     formatted=True)
+    # create energy system and pass nodes
+    es = create_energysystem(nodes.values(), **arguments)
 
-    outputs = results.slice_unstacked(bus_label=cc + '_bus_el', type='output',
-                                      date_from=date_from, date_to=date_to,
-                                      formatted=True)
+    # create optimization model and solve it
+    om = simulate(es=es, **arguments)
 
-    other = results.slice_unstacked(bus_label=cc + '_bus_el', type='other',
-                                    date_from=date_from, date_to=date_to,
-                                    formatted=True)
+    # write results in output directory
+    write_results(es=es, om=om, **arguments)
+    logging.info('Done! \n Check the results')
 
-    # rename redundant columns
-    inputs.rename(columns={cc + '_storage_phs': cc + '_storage_phs_out'},
-                  inplace=True)
-    outputs.rename(columns={cc + '_storage_phs': cc + '_storage_phs_in'},
-                   inplace=True)
-    other.rename(columns={cc + '_storage_phs': cc + '_storage_phs_level'},
-                 inplace=True)
-    inputs.rename(columns={cc + '_reservoir': cc + '_storage_reservoir_out'},
-                  inplace=True)
-    outputs.rename(columns={cc + '_reservoir': cc + '_storage_reservoir_in'},
-                   inplace=True)
-    other.rename(columns={cc + '_reservoir': cc + '_storage_reservoir_level'},
-                 inplace=True)
-    inputs.rename(columns={cc + '_cspstorage': cc + '_storage_csp_out'},
-                  inplace=True)
-    outputs.rename(columns={cc + '_cspstorage': cc + '_storage_csp_in'},
-                   inplace=True)
-    other.rename(columns={cc + '_cspstorage': cc + '_storage_csp_level'},
-                 inplace=True)
+    return
 
-    # data from model in MWh
-    country_data = pd.concat([inputs, outputs, other], axis=1)
 
-    # sort columns and save as csv file
-    file_name = 'scenario_' + nodes_flows.replace('.csv', '_') + date + '_' +\
-                cc + '.csv'
-    country_data.sort_index(axis=1, inplace=True)
-    country_data.to_csv(os.path.join(results_path, file_name))
+###############################################################################
 
-print("The calculations have been completed. Feel free to start the post-processing procedures.")
+if __name__ == '__main__':
+    arguments = docopt(__doc__, version='renpass_gis v0.1')
+    print(arguments)
+    logger.define_logging()
+    main(**arguments)
