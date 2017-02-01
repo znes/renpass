c1563c4e7c3aa0ca85399c48d6dfc503fb47766f
Martin <martin.soethe@uni-flensburg.de>
2017-02-01 18:48:22 +0100

    Replace country codes with bus labels
    Fixes #19

--- renpass_gis_main.py
+++ renpass_gis_main.py
@@ -169,63 +169,38 @@ def write_results(es, om, **arguments):
     results.index = results.reorder_levels([0, 1, 3, 2]).index
     results.sortlevel(inplace=True)
 
+    # get string representation of buses
+    buses = [b.label for b in es.entities if isinstance(b, Bus)]
 
-    for cc in country_codes:
+    for b in buses:
+
         # build single dataframe for electric buses
-        inputs = results.slice_unstacked(bus_label=cc + '_bus_el',
+        inputs = results.slice_unstacked(bus_label=b,
                                          type='to_bus',
                                          date_from=arguments['--date-from'],
                                          date_to=arguments['--date-to'],
                                          formatted=True)
 
-        outputs = results.slice_unstacked(bus_label=(cc + '_bus_el'),
+        outputs = results.slice_unstacked(bus_label=b,
                                           type='from_bus',
                                           date_from=arguments['--date-from'],
                                           date_to=arguments['--date-to'],
                                           formatted=True)
 
-        other = results.slice_unstacked(bus_label=cc + '_bus_el',
+        other = results.slice_unstacked(bus_label=b,
                                         type='other',
                                         date_from=arguments['--date-from'],
                                         date_to=arguments['--date-to'],
                                         formatted=True)
 
-        # AT, DE and LU are treated as one bidding area
-        if cc == 'DE':
-            for c in ['DE', 'AT', 'LU']:
-                # rename redundant columns
-                inputs.rename(columns={c + '_storage_phs':
-                                       c + '_storage_phs_out'},
-                              inplace=True)
-                outputs.rename(columns={c + '_storage_phs':
-                                        c + '_storage_phs_in'},
-                               inplace=True)
-                other.rename(columns={c + '_storage_phs':
-                                      c + '_storage_phs_level'},
-                             inplace=True)
-
-                # data from model in MWh
-                country_data = pd.concat([inputs, outputs, other], axis=1)
-        else:
-            # rename redundant columns
-            inputs.rename(columns={cc + '_storage_phs': cc +
-                                   '_storage_phs_out'},
-                          inplace=True)
-            outputs.rename(columns={cc + '_storage_phs': cc +
-                                    '_storage_phs_in'},
-                           inplace=True)
-            other.rename(columns={cc + '_storage_phs': cc +
-                                  '_storage_phs_level'},
-                         inplace=True)
-
-            # data from model in MWh
-            country_data = pd.concat([inputs, outputs, other], axis=1)
+        # data from model in MWh
+        bus_data = pd.concat([inputs, outputs, other], axis=1)
 
         # sort columns and save as csv file
         file_name = 'scenario_' + os.path.basename(arguments['NODE_DATA'])\
-            .replace('.csv', '_') + date + '_' + cc + '.csv'
-        country_data.sort_index(axis=1, inplace=True)
-        country_data.to_csv(os.path.join(results_path, file_name))
+            .replace('.csv', '_') + date + '_' + b + '.csv'
+        bus_data.sort_index(axis=1, inplace=True)
+        bus_data.to_csv(os.path.join(results_path, file_name))
 
     return
 
