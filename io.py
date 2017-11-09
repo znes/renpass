1fb31c92086a497c93d448d23da91d8d760990a9
Martin <martin.soethe@uni-flensburg.de>
2017-11-09 15:50:01 +0100

    Add example plot

--- io.py
+++ io.py
@@ -1,7 +1,15 @@
 # from plot import Demand, ImportExport etc...
 
-def export_html_report():
+def export_html_report(results):
     # maybe use plotly.offline as described here to generate HTML divs --> second answer
     # and cufflinks for using pandas plotting functionality
     # https://stackoverflow.com/questions/36262748/python-save-plotly-plot-to-local-file-and-insert-into-html
+    head = "<head> </head>"
+    plots = []
+
+    plots.append(plot_prices(results))
+
+    head + div for div in plots
+    # export to html
+
     pass
--- plot.py
+++ plot.py
@@ -1,1 +1,13 @@
 # container for all plotting functions, maybe classes
+
+def plot_prices(results):
+    """
+
+    Returns
+    -------
+    str
+        Plotly div.
+    """
+    results.slice_by(bus_label='DE_bus_el', type='other', obj_label='duals')
+
+    return
