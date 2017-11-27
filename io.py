2c256e40c3df68af8545df4ff4108398af0f2faa
Martin <martin.soethe@uni-flensburg.de>
2017-11-27 18:26:46 +0100

    Implement html export

--- io.py
+++ io.py
@@ -4,23 +4,32 @@ def export_html_report(results):
     # maybe use plotly.offline as described here to generate HTML divs --> second answer
     # and cufflinks for using pandas plotting functionality
     # https://stackoverflow.com/questions/36262748/python-save-plotly-plot-to-local-file-and-insert-into-html
-    
+
     from plot import (get_countrycodes_techs, get_hourly_dispatch, get_transmission,
                       plot_hourly_dispatch, plot_yearly_sums, plot_transmission)
 
-    head = "<head> </head>"
+
     plots = []
 
-    plots.append(plot_prices(results))
+    #plots.append(plot_prices(results))
     countrycodes, techs = get_countrycodes_techs(results)
     timelines = get_hourly_dispatch(countrycodes, techs, results)
     transmission = get_transmission(results)
-    plot_hourly_dispatch(timelines)
-    plot_yearly_sums(timelines, techs)
-    plot_transmission(transmission, countrycodes)
-    
+    plots.append(plot_hourly_dispatch(timelines))
+    plots.append(plot_yearly_sums(timelines, techs))
+    plots.append(plot_transmission(transmission, countrycodes))
 
-    head + div for div in plots
+    head = """ <html><head> <script src="https://cdn.plot.ly/plotly-latest.min.js"></script> </head>"""
+
+    #TODO Something is not working with hourly_dispatch
+    tmp = plots[0]
+    plots = plots[1:]
+
+    html = head + "<body>" + "".join(plots) + "</body></html>"
+
     # export to html
+    f = open('report.html', 'w')
+    f.write(html)
+    f.close()
 
     pass
