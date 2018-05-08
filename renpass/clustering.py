8273813b3f05a73bb909e4f63879898ecd5edaa5
simnh <simon.hilpert@uni-flensburg.de>
2018-05-08 13:15:45 +0200

    Fix period bound constraint and remove obsolete cluster code

--- renpass/clustering.py
+++ renpass/clustering.py
@@ -10,57 +10,6 @@ from datapackage import Package
 import pandas as pd
 import pyomo.environ as po
 
-import tsam.timeseriesaggregation as tsam
-
-
-def temporal_clustering(datapackage, n_cluster):
-    """
-    """
-    package_root_path = os.path.dirname(datapackage)
-    package_json_path = os.path.basename(datapackage)
-
-    p = Package(datapackage)
-
-    sequence_resources = [
-        r for r in p.resources
-        if re.match(r'^data/sequences/.*$', r.descriptor['path'])]
-
-    dfs = {
-        r.name: pd.DataFrame(r.read(keyed='True')).set_index('timeindex').astype(float)
-        for r in sequence_resources}
-    sequences = pd.concat(dfs.values(), axis=1)
-
-    aggregation = tsam.TimeSeriesAggregation(
-        sequences,
-        noTypicalPeriods=n_cluster,
-        rescaleClusterPeriods=False,
-        hoursPerPeriod=24,
-        clusterMethod='hierarchical')
-
-    clustered_sequences = aggregation.createTypicalPeriods()
-
-    cluster_weights = {}
-
-    for n, w in aggregation.clusterPeriodNoOccur.items():
-        cluster_weights[aggregation.clusterCenterIndices[n]] = w
-
-    clustered_timeindex = pd.Series(
-        {d:cluster_weights[d.dayofyear]
-         for d in sequences.index
-         if d.dayofyear in aggregation.clusterCenterIndices}, name='weighting')
-    clustered_timeindex.index.name = 'timeindex'
-
-    for r in sequence_resources:
-        dfs[r.name].loc[clustered_timeindex.index].to_csv(
-            os.path.join(package_root_path, r.descriptor['path']))
-
-    clustered_timeindex.to_csv(
-        os.path.join(package_root_path, 'data', 'temporal.csv'),
-        header=True, sep=";", date_format='%Y-%m-%dT%H:%M:%SZ')
-
-    return clustered_timeindex
-
-
 def temporal_cluster_constraints(m, period_length=24):
     """ Set the storage level of every storage to it's initial capacity
     at every first hour of a cluster period.
@@ -72,8 +21,11 @@ def temporal_cluster_constraints(m, period_length=24):
         m.es.timeindex.get_loc(t + pd.Timedelta(hours=period_length-1))
         for t in m.PERIOD_STARTS}
 
-    m.STORAGES = (m.GenericInvestmentStorageBlock.INVESTSTORAGES |
-                  m.GenericStorageBlock.STORAGES)
+    m.STORAGES = []
+    if hasattr(m, 'GenericInvestmentStorageBlock'):
+        m.STORAGES += [s for s in m.GenericInvestmentStorageBlock.INVESTSTORAGES]
+    if hasattr(m, 'GenericStorageBlock'):
+        m.STORAGES += [s for s in m.GenericStorageBlock.STORAGES]
 
     m.period_bounds = po.Constraint(m.STORAGES, m.START_END.keys())
     def _period_bounds(m):
