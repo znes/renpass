5690d7bf8a65556a3fe0fa29f3808776170044b0
simnh <simon.hilpert@uni-flensburg.de>
2018-08-15 14:06:24 +0200

    Update investment example 2nd

--- renpass/examples/scripting/elements_from_datapackage.py
+++ renpass/examples/scripting/elements_from_datapackage.py
@@ -3,14 +3,24 @@ from oemof.solph import EnergySystem, Model
 import options
 import pprint
 
-es = EnergySystem.from_datapackage(
+es1 = EnergySystem.from_datapackage(
     'examples/dispatch/datapackage.json',
     attributemap={},
     typemap=options.typemap)
 
-for n in es.nodes:
+for n in es1.nodes:
     pprint.pprint(n.__dict__)
 
-m = Model(es)
+
+# investment example
+es2 = EnergySystem.from_datapackage(
+    'examples/investment/datapackage.json',
+    attributemap={},
+    typemap=options.typemap)
+
+for n in es2.nodes:
+    pprint.pprint(n.__dict__)
+
+m = Model(es2)
 
 m.pprint()
