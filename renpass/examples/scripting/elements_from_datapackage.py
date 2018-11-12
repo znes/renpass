9b8ea61b17eeb0d557d3874146989ba35eeea840
simnh <simon.hilpert@uni-flensburg.de>
2018-11-12 13:00:55 +0100

    Improve example for postprocessing

--- renpass/examples/scripting/elements_from_datapackage.py
+++ renpass/examples/scripting/elements_from_datapackage.py
@@ -1,12 +1,10 @@
-
+from oemof.outputlib import views, processing
 from oemof.solph import EnergySystem, Model
+
 from renpass import options
 from renpass import postprocessing as pp
-import pprint
-
 
 dispatch = False
-
 
 if dispatch:
     es = EnergySystem.from_datapackage(
@@ -24,8 +22,9 @@ else:
 es.timeindex = es.timeindex[0:5]
 m = Model(es)
 
-m.pprint()
 m.solve()
+
+m.process_results()
 
 # get component results
 cr = pp.component_results(es, m.results(), select='sequences')
@@ -36,10 +35,22 @@ br = pp.bus_results(es, m.results(), select='scalars')
 # select on bus and reduce multiindex
 br['bus0'].xs([es.groups['bus0'], 'invest'], level=[1, 2])
 
+pp.supply_results(results=m.results(), es=es, bus=['heat-bus'])
+
+pp.supply_results(results=m.results(), es=es, bus=['bus0', 'bus1'])
+
+pp.demand_results(results=m.results(), es=es, bus=['bus0', 'bus1'])
+
+views.node_input_by_type(m.results(), node_type=options.typemap['storage'],
+                         droplevel=[2])
+
+views.node_output_by_type(m.results(), node_type=options.typemap['storage'],
+                         droplevel=[2])
+
+views.node_weight_by_type(m.results(), node_type=options.typemap['storage'])
+
+views.net_storage_flow(results=m.results(), node_type=options.typemap['storage'])
 
 
 
-
-
-
-#supply = cr['load'].xs(['bus0', 'flow'], axis=1, level=[0, 2])
+views.node(processing.parameter_as_dict(es), es.nodes[0], multiindex=True)['scalars']
