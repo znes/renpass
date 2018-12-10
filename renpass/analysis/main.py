950e64dc5fb888d63450c10ff2e2e138eddf52e7
simnh <simon.hilpert@uni-flensburg.de>
2018-12-10 10:18:43 +0100

    Add analysis sub-package

---
+++ renpass/analysis/main.py
@@ -0,0 +1,129 @@
+import os
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_table_experiments as dt
+from matplotlib import colors
+import pandas as pd
+import plotly.graph_objs as go
+
+from renpass import options
+from renpass.analysis import plots
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
+app.config.suppress_callback_exceptions = True
+
+app.datapath = os.path.abspath('../../data/oemof-eu/results')
+app.scenarios = os.listdir(app.datapath)
+app.color_dict = {
+    name: colors.to_hex(color) for name, color in options.techcolor.items()}
+
+
+app.layout = html.Div([
+    dcc.Location(id='url', refresh=False),
+    html.Div(id='page-content')
+])
+
+index_page = html.Div([
+    html.H1(children='Renpass Results Analysis'),
+
+    dcc.Link('Analysis', href='/analysis'),
+
+])
+
+
+analysis_layout = html.Div(children=[
+    html.H1(children='Renpass Results Analysis'),
+
+    dcc.Link('Go back to startpage', href='/', className='six columns'),
+
+    html.Div([
+        html.Div([
+            html.Label('Scenarios'),
+            dcc.Dropdown(
+                id='scenario',
+                options=[{
+                    'label': s,
+                    'value': s}
+                         for s in app.scenarios],
+                value=''
+            )],
+        style={'width': '20%'})
+    ]),
+
+    html.Div([
+        dcc.Input(
+            id='country',
+            placeholder='Enter a country code...',
+            type='text',
+            value='DE'
+        )
+    ], style={'width': '20%'}),
+
+    html.Div(children=[
+        dcc.Graph(
+            id='hourly_plot',
+            figure = dict(
+                layout = go.Layout(
+                    barmode='stack',
+                    title='Hourly Plot',
+                    yaxis=dict(
+                        title='Energy in MWh',
+                        titlefont=dict(
+                            size=16,
+                            color='rgb(107, 107, 107)'
+                        ),
+                        tickfont=dict(
+                            size=14,
+                            color='rgb(107, 107, 107)'
+                        )
+                    )
+                )
+            )
+        )
+    ], className='six Columns'),
+
+    html.Div(children=[
+        dcc.Graph(
+            id='stacked_plot'
+        )
+    ], className='six Columns'),
+
+
+
+])
+
+
+
+@app.callback(dash.dependencies.Output('page-content', 'children'),
+              [dash.dependencies.Input('url', 'pathname')])
+def display_page(pathname):
+    if pathname == '/analysis':
+         return analysis_layout
+    else:
+        return index_page
+    # You could also return a 404 "URL not found" page here
+
+@app.callback(
+    dash.dependencies.Output('hourly_plot', 'figure'),
+    [dash.dependencies.Input('scenario', 'value'),
+     dash.dependencies.Input('country', 'value')])
+def update_hourly_plot(scenario, country='DE'):
+    """
+    """
+    return plots.hourly_plot(scenario, country)
+
+@app.callback(dash.dependencies.Output('stacked_plot', 'figure'),
+              [dash.dependencies.Input('scenario', 'value')])
+def update_stacked_plot(scenario):
+    """
+    """
+    return plots.stacked_plot(scenario)
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
---
+++ renpass/analysis/plots.py
@@ -0,0 +1,122 @@
+# -*- coding: utf-8 -*-
+import json
+import os
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_table_experiments as dt
+
+import plotly.plotly as py
+import plotly.graph_objs as go
+import pandas as pd
+
+from renpass import options
+from renpass.analysis.main import app
+
+def hourly_plot(scenario, country):
+    """
+    """
+    datapath = app.datapath
+
+    df = pd.read_csv(
+        os.path.join(datapath, scenario, 'supply-' + country + '-electricity.csv'),
+        index_col=[0], parse_dates=True)
+
+    load = pd.read_csv(
+        os.path.join(datapath, scenario, 'load.csv'),
+        index_col=[0], parse_dates=True)[country +'-electricty-load']
+
+    #df = df.resample('1D').mean()
+    x = df.index
+    df.columns = [c.strip(country + '-') for c in df.columns]
+
+    flexibility = ['import', 'acaes', 'phs', 'lithium_battery']
+
+    # create plot
+    layout = go.Layout(
+        barmode='stack',
+        title='Hourly supply and demand in {} for scenario {}'.format(country, scenario),
+        yaxis=dict(
+            title='Energy in MWh',
+            titlefont=dict(
+                size=16,
+                color='rgb(107, 107, 107)'
+            ),
+            tickfont=dict(
+                size=14,
+                color='rgb(107, 107, 107)'
+            )
+        )
+    )
+
+    data = []
+
+
+    for c in df:
+        if c not in flexibility:
+            data.append(
+                go.Scatter(
+                    x = x,
+                    y = df[c],
+                    name=c,
+                    stackgroup='positive',
+                    line=dict(width=0, color=app.color_dict[c])
+                )
+            )
+        else:
+            data.append(
+                go.Scatter(
+                    x = x,
+                    y = df[c].clip(lower=0),
+                    name=c,
+                    stackgroup='positive',
+                    line=dict(width=0, color=app.color_dict.get(c, 'black'))
+                )
+            )
+            data.append(
+                go.Scatter(
+                    x = x,
+                    y = df[c].clip(upper=0),
+                    name=c+'-charge',
+                    stackgroup='negative',
+                    line=dict(width=0, color=app.color_dict.get(c, 'black')),
+                    showlegend= False
+                )
+            )
+
+    # append load
+    data.append(
+        go.Scatter(
+            x = x,
+            y = load,
+            name = load.name,
+            line=dict(width=3, color='darkred')
+        )
+    )
+
+    return {'data': data, 'layout': layout}
+
+
+def stacked_plot(scenario):
+    """
+    """
+    df = pd.read_csv(
+        os.path.join(app.datapath, scenario, 'capacities.csv'),
+        index_col=0)
+
+    return {
+        'data': [go.Bar(
+            x = row.index,
+            y = row.values,
+            name = idx,
+            marker=dict(color=app.color_dict[idx])
+       ) for idx, row in df.iterrows()],
+       'layout': go.Layout(
+            barmode='stack',
+            title="Installed capacities for scenario {}".format(scenario)
+       )
+    }
+
+
+#datapath = os.path.abspath('../../data/oemof-eu/results')
